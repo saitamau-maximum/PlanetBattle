97fce6a4b8da4d905619e213c78fce98df0a9c10
Yuto <naturalwater56@gmail.com>
2026-03-27 17:52:37 +0900

    feat:敵のランダムすぽなーを追加

---
+++ Assets/Script/Enemy/Spawner/RandomEnemySpawner.cs
@@ -0,0 +1,32 @@
+using UnityEngine;
+using Utility;
+
+public class RandomEnemySpawner : MonoBehaviour
+{
+    [SerializeField] private EnemyController[] _enemyPre;
+    [SerializeField] private float _maxCoolTime;
+    [SerializeField] private float _minCoolTime;
+    [SerializeField] private int _maxEnemyCount;
+
+    [SerializeField] private Transform _target;
+    private CountdownTimer _coolTimer = new(0);
+
+    private void Update()
+    {
+        _coolTimer.Tick();
+
+        if (_coolTimer.IsFinished() && transform.childCount <= _maxEnemyCount)
+        {
+            EnemyController enemy = Instantiate(_enemyPre[Random.Range(0, _enemyPre.Length)], transform.position, transform.rotation, transform);
+            enemy.Init(_target);
+
+            SetCoolTimer();
+        }
+    }
+
+    private void SetCoolTimer()
+    {
+        _coolTimer.Reset(Random.Range(_minCoolTime, _maxCoolTime));
+        _coolTimer.Start();
+    }
+}
