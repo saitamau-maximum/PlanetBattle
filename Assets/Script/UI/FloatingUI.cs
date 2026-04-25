48ddcd12fcb998aac122bf53f6bbb09a817471d3
Yuto <naturalwater56@gmail.com>
2026-04-26 00:31:36 +0900

    fix:いらない変数を削除

--- Assets/Script/UI/FloatingUI.cs
+++ Assets/Script/UI/FloatingUI.cs
@@ -6,15 +6,13 @@ public class FloatingUI : MonoBehaviour
     [SerializeField] private float _frequency = 1f;    // 周波数（往復速度）
 
     private Vector3 _baseLocalPos;
-    private Transform _parent;
 
     private void Awake()
     {
         _baseLocalPos = transform.localPosition;
-        _parent = transform.parent;
     }
 
-    private void LateUpdate()
+    private void Update()
     {
         transform.rotation = Quaternion.identity;
         // --- 上下にゆっくり移動（ローカル基準） ---
