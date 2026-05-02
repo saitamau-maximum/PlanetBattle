1bda6416c1f39e0a08ef579b4ebbf1bc9e9865ce
Yuto <naturalwater56@gmail.com>
2026-05-02 23:43:23 +0900

    fix:BuildingBoxを破壊させるように修正

--- Assets/Script/StructureBuilding/BuildingBox.cs
+++ Assets/Script/StructureBuilding/BuildingBox.cs
@@ -5,6 +5,18 @@ public class BuildingBox : MonoBehaviour
 {
     private GameObject _structurePrefab;
     private CountdownTimer _buildTimer;
+    private Health _health;
+    protected void Awake()
+    {
+        _health = GetComponent<Health>();
+    }
+    protected void Start()
+    {
+        if (_health != null)
+        {
+            _health.OnDied += Die;
+        }
+    }
 
     public void Init(StructureData data)
     {
@@ -25,5 +37,18 @@ public class BuildingBox : MonoBehaviour
             Instantiate(_structurePrefab, transform.position, Quaternion.identity);
             Destroy(gameObject);
         }
+    }
+
+    private void OnDestroy()
+    {
+        if (_health != null)
+        {
+            _health.OnDied -= Die;
+        }
+    }
+
+    private void Die()
+    {
+        Destroy(gameObject);
     }
 }
