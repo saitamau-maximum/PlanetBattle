7c1374ecc1c5149b9560e58cdc4a103e25104178
Yuto <naturalwater56@gmail.com>
2026-05-02 19:41:51 +0900

    fix:微調整

--- Assets/Script/UI/HealthBarCanvas.cs
+++ Assets/Script/UI/HealthBarCanvas.cs
@@ -5,19 +5,33 @@ public class HealthBarCanvas : MonoBehaviour
 {
     [SerializeField] private Health _health;
     [SerializeField] private Image _healthBarFill;
+    [SerializeField] private float _speed = 5f; // 大きいほど速い
+
+    private float _targetRatio;
 
     private void Start()
     {
-        _health.OnHealthChanged += UpdateHealthBar;
-        UpdateHealthBar(_health.HealthRatio);
-    }
-    private void OnDestroy()
-    {
-        _health.OnHealthChanged -= UpdateHealthBar;
+        _health.OnHealthChanged += OnHealthChanged;
+        _targetRatio = _health.HealthRatio;
+        _healthBarFill.fillAmount = _targetRatio;
     }
 
-    private void UpdateHealthBar(float ratio)
+    private void OnDestroy()
     {
-        _healthBarFill.fillAmount = ratio;
+        _health.OnHealthChanged -= OnHealthChanged;
+    }
+
+    private void Update()
+    {
+        _healthBarFill.fillAmount = Mathf.Lerp(
+            _healthBarFill.fillAmount,
+            _targetRatio,
+            Time.deltaTime * _speed
+        );
+    }
+
+    private void OnHealthChanged(float ratio)
+    {
+        _targetRatio = ratio;
     }
 }
