068edc87deb79b2bdfd45cad409afc629ba40674
Yuto <naturalwater56@gmail.com>
2026-05-15 02:58:07 +0900

    fix:敵が倒されてもゲージが減らない問題の修正

--- Assets/Script/Enemy/Spawner/WaveEnemySpawner.cs
+++ Assets/Script/Enemy/Spawner/WaveEnemySpawner.cs
@@ -8,16 +8,18 @@ public class WaveEnemySpawner : MonoBehaviour
     [SerializeField] private int _firstIndex = 0;
     [SerializeField] private WaveData _waveData;
 
+    private int _maxEnemyCount = 0;
     private int _killedEnemyCount = 0;
     private float _timer = 0;
     private int _enemyIndex = 0;
 
-    public float ProgressRatio => 1 - (_waveData.EnemyList.Length == 0 ? 0 : Mathf.Clamp01((float)_killedEnemyCount / _waveData.EnemyList.Length));
+    public float ProgressRatio => 1 - ((float)_killedEnemyCount / _maxEnemyCount);
     public event Action<float> OnProgressChanged;
 
     private void Start()
     {
         _enemyIndex = _firstIndex;
+        _maxEnemyCount = _waveData.EnemyList.Length != 0 ? _waveData.EnemyList.Length : 1;
     }
 
     private void Update()
@@ -31,15 +33,19 @@ public class WaveEnemySpawner : MonoBehaviour
         {
             GameObject enemy = Instantiate(spawnInfo.EnemyPrefab, transform.position, transform.rotation, transform);
             enemy.GetComponent<CharacterAIController>().Init(_target);
-            enemy.GetComponent<Health>().OnDied += () => { _killedEnemyCount++; };
+            enemy.GetComponent<Health>().OnDied += OnEnemyKilled;
 
             _enemyIndex++;
-            OnProgressChanged?.Invoke(ProgressRatio);
 
             _timer = 0;
         }
     }
 
+    private void OnEnemyKilled()
+    {
+        _killedEnemyCount++;
+        OnProgressChanged?.Invoke(ProgressRatio);
+    }
     private bool CheckStageClear()
     {
         if (transform.childCount <= 0)
